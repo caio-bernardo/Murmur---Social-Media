966639e3beb04af018f81b3da6ac94698dc5b847
Caio Bernardo <caio.2004vb@gmail.com>
2025-06-19 16:52:17 -0300

    feat: Add comments router to API

--- src/app/api.py
+++ src/app/api.py
@@ -6,12 +6,15 @@ from ninja import NinjaAPI
 # Import routers
 from accounts.views import router as accounts_router
 from posts.views import router as posts_router
+from comments.views import router as comments_router
 
 app = NinjaAPI()
 
 app.add_router("/token", tags=["Auth"], router=obtain_pair_router)
 app.add_router("/accounts", tags=["Account"], router=accounts_router)
 app.add_router("/posts", tags=["Posts"], router=posts_router)
+app.add_router("/comments", tags=["Comment"], router=comments_router)
+
 
 @app.get("/")
 async def checkhealth(request):
