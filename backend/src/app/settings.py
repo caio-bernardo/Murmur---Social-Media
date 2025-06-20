2d5fb2c9db9329e40bec8af31e2032787a6c9199
Caio Bernardo <caio.2004vb@gmail.com>
2025-06-20 19:16:17 -0300

    feat: Add Docker, Docker Compose, and Nginx configuration
    This commit adds the necessary files to containerize the Django
    application using Docker and Docker Compose, including:
    
    - Dockerfile for building the backend image - docker-compose.yml for
    defining and managing the services - nginx.conf for serving static files
    and proxying requests to the backend - .dockerignore to exclude
    unnecessary files from the image - Updated settings.py to use PostgreSQL
    database.

--- backend/src/app/settings.py
+++ backend/src/app/settings.py
@@ -45,7 +45,7 @@ NINJA_JWT = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
@@ -102,8 +102,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST", "localhost"),
+        "PORT": os.environ.get("DATABASE_PORT", "5432"),
     }
 }
 
